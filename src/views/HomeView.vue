732414614a6e79b35581ae2262b6e2afa3a399f5
Joseph Rey Tolentino <72607892+abi-collab@users.noreply.github.com>
2024-11-14 12:27:24 +0800

    setups

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import ArticleList from './HomeViewSections/ArticleList.vue';
+import Header from './HomeViewSections/Header.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="space-y-16">
+    <Header />
+    <div class="container mx-auto border border-red-700">
+      <ArticleList />
+    </div>
   </main>
 </template>
